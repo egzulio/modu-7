ffe1ac5f1652dd2cbf4ae3cb940ac88c2dcf8864
egzulio <miszka.dylewicz@gmail.com>
2017-06-02 19:58:55 +0200

    Zadanie 8.4

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+// to jest jeden z komentarzy 
+
+/* to jest drugi komentarz */
+
+/* to jest ostatni
+                    komentarz
+                                dzisiaj */
