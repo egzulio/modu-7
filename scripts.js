0e5d102cd1eb094590415cc639d94a28716c6dd6
egzulio <miszka.dylewicz@gmail.com>
2017-06-04 15:40:11 +0200

    Dodanie zadania 8.5

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-// scripts.js
+var a = 12
+var h = 10
+var areaOfTriangle = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + areaOfTriangle);
 
-var a = 12,
-var h = 10,
-var areaOfTriangle = a*h/2,
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea)
+var question = prompt('Ile cm2 wynosi pole trójkąta o boku 12 cm i wysokości 10 cm?');
+
+switch (question.toLowerCase()) {
+  case '60':
+    alert('Brawo, to jest prawidłowy wynik!');
+    break;
+  default:
+    alert('Niestety nie :(');
+}
