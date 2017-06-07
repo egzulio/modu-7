70bb995e1dc22879bd1b9b5e519bf6de7743f091
egzulio <miszka.dylewicz@gmail.com>
2017-06-07 18:02:23 +0200

    Dodałem zadanie 9.1

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
-var a = 12
-var b = 25
-var dzialanie = (a * a) + (2 * a * b) - (b * b)
-console.log('Kiedy pomnożymy ' + a + ' przez ' + a +', dodając do tego ' + a + ' x ' + b + ' x 2 oraz odejmując ' + b + ' podniesione do kwadratu otrzymamy ' + dzialanie);
 
 
-if (dzialanie > 0) {
-    console.log('Wynik jest dodatni');
-} else if (dzialanie < 0) {
-    console.log('Wynik jest ujemny');
-} else {
-    console.log('Wynik jest równy zeru');
+
+function getTriangleArea(a, h) {
+    var dzialanie = a * h / 2
+    if (h <= 0) {
+    console.log('Nieprawidłowe dane');
+} else if (a <= 0) {
+    console.log('Nieprawidłowe dane');
+   return (dzialanie);
+}
+}
+console.log( getTriangleArea(10,6) )
 }
