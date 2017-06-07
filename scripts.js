53407e421f1c7a227d1fa6f5170d9eafe913c46d
egzulio <miszka.dylewicz@gmail.com>
2017-06-07 18:41:10 +0200

    Dodałem zadanie 9.1 - poprawione

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
-
-
-
 function getTriangleArea(a, h) {
     var dzialanie = a * h / 2
     if (h <= 0) {
     console.log('Nieprawidłowe dane');
 } else if (a <= 0) {
     console.log('Nieprawidłowe dane');
-   return (dzialanie);
+}
+    else{
+    return dzialanie;
 }
 }
-console.log( getTriangleArea(10,6) )
-}
+
+console.log(getTriangleArea(10,6));
