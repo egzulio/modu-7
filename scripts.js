4b41e7dcde261a1b1558afd0d3e9ace5bcb915bb
egzulio <miszka.dylewicz@gmail.com>
2017-06-11 15:45:16 +0200

    Dodałem zadanie 10.2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
-var withMenuItemsClass = document.getElementsByClassName('button');
-for ( var withMenuItemsClass = 2; withMenuItemsClass < 3 ; withMenuItemsClass++; withMenuItemsClass.innerText = 'Jeszcze raz';) 
-var array = ['HTML', 2, 'World', 9.99, {name: 'Piotr'}, 99];
-console.log(withMenuItemsClass);
+$(document).ready(function(){
+	console.log('DOM loaded - you can have fun');
+});
+var span = $("span");
+    span.each(function(index, element) {
+
+ $("span:even").css('color', 'red');
+
+});
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button) 
+
+});
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
