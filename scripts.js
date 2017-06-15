9419b63cba03563ac3e11b1beb4bbcbaaf515658
egzulio <miszka.dylewicz@gmail.com>
2017-06-15 20:13:56 +0200

    poprawiłem. nie patrz na buttony bo wrzucilem je tylko zeby zobaczyc czy slajdy mi sie zmienia

--- scripts.js
+++ scripts.js
@@ -1,18 +1,39 @@
 $(function(){
-	//this code will execute after the DOM is loaded
-});
-var carouselList = $("#carousel ul");$
-setTimeout(changeSlide, 4000);
-setInterval(changeSlide, 4000); 
-carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-
-
-
-moveFirstSlide = 
+var carouselList = $('.slides');
+  
     
-var firstItem = carouselList.find("li:first");
-var lastItem = carouselList.find("li:last");
+function changeRightSlide(){
+    carouselList.animate({'margin-left': -800}, 500, moveFirstSlide);
+  };
 
-lastItem.after(firstItem)
+function changeLeftSlide(){
+    moveLastSlide();
+    carouselList.animate({'margin-left': 0}, 500);
+  };
 
-carouselList.css({marginLeft:0});
+function moveFirstSlide(){
+    var firstItem = carouselList.find('li:first');
+    var lastItem = carouselList.find('li:last');
+    $('.carousel2 li.active').removeClass('active').next().add('.carousel2 li:first').last().addClass('active');
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft: 0});
+  };
+
+function moveLastSlide(){
+    var firstItem = carouselList.find('li:first');
+    var lastItem = carouselList.find('li:last');
+    $('.carousel2 li.active').removeClass('active').prev().add('.carousel2 li:last').first().addClass('active');
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft: -800});
+  };
+    
+    
+    
+    
+       $('.right-btn').click(function(){
+    changeRightSlide();
+  });
+
+  $('.left-btn').click(function(){
+    changeLeftSlide();
+  });
